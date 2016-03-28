7ba66b46a1636124a8a897f48bc3372b5a02354c
yair staszewski <ymsil719@gmail.com>
2016-03-28 15:23:15 +0300

    try now

---
+++ FMS5776_2075_3798/FMS5776_2075_3798/FCB.cpp
@@ -0,0 +1,48 @@
+#include "FCB.h"
+
+
+
+FCB::FCB()
+{
+}
+
+FCB::FCB(Disk *)
+{
+}
+
+
+FCB::~FCB()
+{
+}
+
+void FCB::closefile()
+{
+}
+
+void FCB::flushfile()
+{
+}
+
+void FCB::readRecord(char *, unsigned int)
+{
+}
+
+void FCB::writeRecord(char *)
+{
+}
+
+void FCB::seek(unsigned int, int)
+{
+}
+
+void FCB::updateCancel()
+{
+}
+
+void FCB::deleteRecord()
+{
+}
+
+void FCB::updateRecord(char *)
+{
+}
